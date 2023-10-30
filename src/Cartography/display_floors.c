66c03b6337f0164db7d2e086393d7acf1de21a60
goodfoodtruck <mielatomique@protonmail.com>
2023-10-30 12:59:36 +0100

    #10 clear prompt and recursive display

--- src/Cartography/display_floors.c
+++ src/Cartography/display_floors.c
@@ -1,7 +1,24 @@
 #include "Doomdepths.h"
 
 void display_floors(Floor *last_floor, Position *player_pos) {
-    printf("\n");
+    system("clear");
+    printf("==[ MAP ]==\n\n");
+
     reverse_display_floors(last_floor, player_pos);
-    printf("\n");
+
+    /* Affichage du menu */
+    char prompt_choice;
+    int choice;
+
+    printf("\n\n0. Exit map\n");
+    scanf(" %c", &prompt_choice);
+    choice = (int)(prompt_choice - '0');
+
+    switch(choice) {
+        case 0:
+            break;
+        default:
+            display_floors(last_floor, player_pos);
+            break;
+    }
 }
